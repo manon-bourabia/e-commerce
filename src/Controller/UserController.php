38212fea3af71273edf8431d922c3c6022072684
Manondev11 <manon.elm@hotmail.com>
2026-02-27 11:52:23 +0100

    add role editor

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,5 +17,22 @@ final class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'users' =>$userRepository->findAll(),
         ]);
+    }
+    #[Route('/admin/user/{id}/to/editor', name: 'app_user_to_editor')] 
+    public function changeRole(EntityManagerInterface $entityManager, User $user): Response{
+        $user ->setRoles(['ROLE_EDITOR', 'ROLE_USER']).
+        $entityManager->flush();
+
+        $this->addFlash('success', "Le rôle éditeur à bien été ajouté à l'utilisateur");
+        return $this->redirectToRoute('app_user');
+    }
+    #[Route('/admin/user/{id}/remove/editor/role', name: 'app_user_remove_editor_role')]
+    public function removeRoleEditor(EntityManagerInterface $entityManager, User $user): Response{
+        $user ->setRoles([]).
+        $entityManager->flush();
+
+        $this->addFlash('success', "Le rôle éditeur à bien été retiré à l'utilisateur");
+        
+        return $this->redirectToRoute('app_user');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -137,4 +137,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function __toString(): string{
+        return $this->email;
+    }
 }
