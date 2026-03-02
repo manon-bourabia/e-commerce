8b1a38ba5d0021887dfb283b8d507e1ec35f6b39
Manondev11 <manon.elm@hotmail.com>
2026-03-02 09:02:46 +0100

    correction remove role

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,34 +14,60 @@ final class UserController extends AbstractController
     #[Route('/admin/user', name: 'app_user')]
     public function index(UserRepository $userRepository): Response
     {
+
+        $users = $userRepository -> findAll();
         return $this->render('user/index.html.twig', [
-            'users' =>$userRepository->findAll(),
+            // 'controller_name' => 'UserController',
+            'users' => $users
         ]);
     }
-    #[Route('/admin/user/{id}/to/editor', name: 'app_user_to_editor')] 
-    public function changeRole(EntityManagerInterface $entityManager, User $user): Response{
-        $user ->setRoles(['ROLE_EDITOR', 'ROLE_USER']).
+    #[Route('/admin/user/{id}/to/editor', name: 'app_user_to_editor')]
+    public function changeRoleToEditor(EntityManagerInterface $entityManager, User $user): Response
+    {
+        $user->setRoles(['ROLE_EDITOR', 'ROLE_USER']);
         $entityManager->flush();
 
-        $this->addFlash('success', "Le rôle éditeur à bien été ajouté à l'utilisateur");
+        $this->addFlash('success', 'Le rôle de l\'utilisateur a été changé avec succès.');
         return $this->redirectToRoute('app_user');
     }
-    #[Route('/admin/user/{id}/remove/editor/role', name: 'app_user_remove_editor_role')]
-    public function removeRoleEditor(EntityManagerInterface $entityManager, User $user): Response{
-        $user ->setRoles([]).
-        $entityManager->flush();
+    #[Route('/admin/user/{id}/change-role/{role}', name: 'app_user_change_role')]
+    public function changeRole(EntityManagerInterface $entityManager, User $user, $role): Response
+    {
+        $validRoles = ['ROLE_EDITOR', 'ROLE_USER'];
 
-        $this->addFlash('success', "Le rôle éditeur à bien été retiré à l'utilisateur");
+        if (!in_array($role, $validRoles, true)) {
+            $this->addFlash('error', "Le rôle demandé n'est pas valide.");
+            return $this->redirectToRoute('app_user');
+        }
+        
+        if ($role !== 'ROLE_USER') {
+            $user->setRoles([$role, 'ROLE_USER']);
+        } else {
+            $user->setRoles([$role]);
+        }
+        $entityManager->flush();
+        
+        $this->addFlash('success', "Le rôle $role a bien été attribué à l'utilisateur.");
         
         return $this->redirectToRoute('app_user');
     }
-    #[Route('/admin/user/{id}/remove/', name: 'app_user_remove')]
-    public function userRemove(EntityManagerInterface $entityManager, $id, UserRepository $userRepository): Response
+     #[Route('/admin/user/{id}/remove/editor/role ', name: 'app_user_remove_editor_role')]
+    public function removeRoleEditor(EntityManagerInterface $entityManager, User $user): Response
     {
-        $user =$userRepository->find($id);
+        $user->setRoles([]);
+        $entityManager->flush();
+
+        $this->addFlash('danger', "Le rôle éditeur à bien été retiré à l'utilisateur");
+        
+        return $this->redirectToRoute('app_user');
+    }
+     #[Route('/admin/user/{id}/remove/', name: 'app_user_remove')]
+    public function ruserRemove(EntityManagerInterface $entityManager,$id,  UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
         $entityManager->remove($user);
         $entityManager->flush();
-        
+
         $this->addFlash('danger', "L'utilisateur à bien été supprimé.");
         
         return $this->redirectToRoute('app_user');
