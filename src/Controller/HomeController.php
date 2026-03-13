a63ab4bfe2138149bb7e85078e6b2e0c86b09c5b
Manondev11 <manon.elm@hotmail.com>
2026-03-13 11:46:00 +0100

    add categories navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,21 +12,23 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
         $products = $productRepository->findAll(); 
         
         return $this->render('home/index.html.twig', [
-            'products'=>$products
+            'products'=>$products,
+            'categories'=>$categoryRepository->findAll()
         ]);
     }
     #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
-    public function showProduct(Product $product, ProductRepository $productRepository): Response
+    public function showProduct(Product $product, ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
-        $lastProductsAdd = $productRepository->findBy([], ['id'=>'DESC'],5);
+        $lastProductsAdd = $productRepository->findBy([], ['id'=>'DESC'],3);
         
         return $this->render('home/show.html.twig', [
             'product'=>$product,
+            'categories'=>$categoryRepository->findAll(),
             'products'=>$lastProductsAdd
         ]);
     }
