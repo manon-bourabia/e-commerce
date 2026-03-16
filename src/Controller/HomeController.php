dac4e22ca21b288c06dd36d7ccc85735e334f574
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-03-16 14:14:59 +0100

    feat: implement pagination for product listings and update filter view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,16 +6,24 @@ use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use App\Repository\SubCategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        $data = $productRepository->findby([], ['id'=>'DESC']);
         $products = $productRepository->findAll(); 
+        $products = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            3
+        );
         
         return $this->render('home/index.html.twig', [
             'products'=>$products,
@@ -34,14 +42,14 @@ final class HomeController extends AbstractController
         ]);
     }
     #[Route('product/subcategory/{id}/filter', name: 'app_home_product_filter', methods: ['GET'])]
-    public function filter($id, SubCategoryRepository $subCategoryRepository): Response
+    public function filter($id, SubCategoryRepository $subCategoryRepository, CategoryRepository $categoryRepository): Response
     {
         $products = $subCategoryRepository->find($id)->getProduct();
         $subCategory = $subCategoryRepository->find($id);
         return $this->render('home/filter.html.twig', [
             'products'=>$products,
-            'subCategory'=>$subCategory,
-            'categories'=>$subCategoryRepository->findAll(),
+            'subCategories'=>$subCategory,
+            'categories'=>$categoryRepository->findAll(),
         ]);
     }
 }
