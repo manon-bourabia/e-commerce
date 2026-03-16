8165b9196da61a533cb3a02152ac791330c75cbe
Manondev11 <manon.elm@hotmail.com>
2026-03-16 13:28:51 +0100

    add pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,22 +6,31 @@ use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use App\Repository\SubCategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
-    {
-        $products = $productRepository->findAll(); 
-        
-        return $this->render('home/index.html.twig', [
-            'products'=>$products,
-            'categories'=>$categoryRepository->findAll()
-        ]);
-    }
+   public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, Request $request, PaginatorInterface $paginator): Response
+{
+    $data = $productRepository->findBy([], ['id' => 'DESC']);
+
+    $products = $paginator->paginate(
+        $data,
+        $request->query->getInt('page', 1),
+        8
+    );
+
+    return $this->render('home/index.html.twig', [
+        'products' => $products,
+        'categories' => $categoryRepository->findAll(),
+        'pagination' => $products
+    ]);
+}
     #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
     public function showProduct(Product $product, ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
