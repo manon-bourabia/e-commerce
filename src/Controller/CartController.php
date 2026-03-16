59d53e71ebb4025569220b5aa953e3bcb830f873
Manondev11 <manon.elm@hotmail.com>
2026-03-16 16:04:15 +0100

    add CartController

---
+++ src/Controller/CartController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CartController extends AbstractController
+{
+    #[Route('/cart', name: 'app_cart')]
+    public function index(SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+        $cartWithData = [];
+        foreach ($cart as $id => $quantity) {
+            $product = $this->productRepository->find($id);
+            if ($product) {
+                $cartWithData[] = [
+                    'product' => $this->productRepository->find($id),
+                    'quantity' => $quantity
+                ];
+            }
+        }
+        $total = array_sum(array_map(function ($item) {
+            return $item['product']->getPrice() * $item['quantity'];
+        }, $cartWithData));
+
+        return $this->render('cart/index.html.twig', [
+            'items' => $cartWithData,
+            'total' => $total
+        ]);
+    }
+    public function __construct(private readonly ProductRepository $productRepository) {}
+
+    #[Route("/cart/add/{id}", name: "app_cart_new", methods: ['GET'])]
+    public function addProductToCart(int $id, SessionInterface $session): Response
+    {
+        $cart = $session->get('cart', []);
+        
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+        
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('app_cart');
+    }
+}
