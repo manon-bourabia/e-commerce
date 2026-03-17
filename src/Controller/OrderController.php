ba6062cbe666762f160fc9b7c65b1299afb473b7
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-03-17 14:02:51 +0100

    maj city controller et form order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
